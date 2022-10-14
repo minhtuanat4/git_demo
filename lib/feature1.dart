60d6194b9e34813314516d0399debb21d33e639c
Trần Minh Tuấn <tuantm@vtc.vn>
2022-10-14 16:50:20 +0700

    Add Column to Feature1

---
+++ lib/feature1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Feature1 extends StatelessWidget {
+  const Feature1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        SizedBox(
+          height: 20,
+        ),
+        Text("Feature1")
+      ],
+    );
+  }
+}
